7eb5f194c6225c23cfc3f447ece68bd139c64439
SuryaTyagi24 <tyagidivyam24@gmail.com>
2024-06-07 16:21:20 +0530

    Your commit message here

---
+++ src/components/Home.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import HeroImage from "../assets/heroImage.png";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
+
+const Home = () => {
+  return (
+    <div
+      name="home"
+      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+    >
+      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+        <div className="flex flex-col justify-center h-full">
+          <h2 className="text-4xl sm:text-7xl font-bold text-white">
+            I'm a Frontend Developer
+          </h2>
+          <p className="text-gray-500 py-4 max-w-md">
+          I'm Suryadev Tyagi, a B.Tech student specializing in Computer Science and Engineering at Galgotias University. 
+          I have experience in building and designing software, and I am passionate about creating and optimizing web applications. I enjoy working with technologies such as React, Tailwind, Next.js, and JavaScript. 
+          My dedication to continuous learning and delivering high-quality work drives impactful user experiences.
+          </p>
+
+          <div>
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
+              Portfolio
+              <span className="group-hover:rotate-90 duration-300">
+                <MdOutlineKeyboardArrowRight size={25} className="ml-1" />
+              </span>
+            </Link>
+          </div>
+        </div>
+
+        <div>
+          <img
+            src={HeroImage}
+            alt="my profile"
+            className="rounded-2xl mx-auto w-2/3 md:w-full"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
